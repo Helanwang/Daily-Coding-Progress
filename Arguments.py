e1591f1adb8452fe419c86f9b4e00e01a416f257
helanwang <christylan1121@gmail.com>
2024-07-24 11:32:26 -0700

    1

---
+++ Arguments.py
@@ -0,0 +1,45 @@
+def grow(name, tall: int):
+    print(f"{name} is  {tall} cm this year")
+
+
+grow("Jack", 189)
+
+
+def my_functions(name):
+    print(f"my function name is  {name}")
+
+
+my_functions("Email")
+my_functions("girls")
+my_functions("Helen")
+
+
+def add(a, b, c):
+    return a + b + c
+
+
+print(add(455345, 45, 45))
+
+
+def calculate_average(num1, num2, num3):
+    average = (num1 + num2 + num3) / 3
+    return average
+
+
+print(f"the average is: {calculate_average(1, 2, 3)}")
+print(f"the average is: {calculate_average(2, 3, 4)}")
+
+
+def calculation(a, b):
+    return a + b
+
+
+exa = calculation(10, 5)
+print(exa)
+
+
+def calculation1(name, name1):
+    print(f"the name of them is{name}, and {name1}")
+
+
+calculation1("jack", "Mike")

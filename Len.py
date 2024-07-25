31d9b24bd5ee2558b17ef8d0c11d071f51917e3c
helanwang <christylan1121@gmail.com>
2024-07-24 22:15:18 -0700

    Notes

---
+++ Len.py
@@ -0,0 +1,14 @@
+letter = "Hello World!"
+print(len(letter))
+
+my_tuple = ("apple", "banana", "cherry")
+print(len(my_tuple))
+
+dictionary = {"apple": 1, "banana": 2, "cherry": 3, "Peach": 5}
+print(len(dictionary))
+
+"""
+12
+3
+4
+"""

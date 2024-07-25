b5fc0e4e1cb9507fc0aed6e860f8ba9aa6257c11
helanwang <christylan1121@gmail.com>
2024-07-24 22:57:04 -0700

    Notes

---
+++ Dictionary.py
@@ -0,0 +1,21 @@
+# Dictionary store data values in pairs.
+
+dictionary1 = {"brand": "Ford",
+               "model": "Mustang",
+               "year": 1999,
+               }
+print(dictionary1)
+print(dictionary1["year"])
+print(dictionary1["model"])
+print(dictionary1["brand"])
+print(len(dictionary1))
+print(type(dictionary1))
+
+"""
+{'brand': 'Ford', 'model': 'Mustang', 'year': 1999}
+1999
+Mustang
+Ford
+3
+<class 'dict'>
+"""

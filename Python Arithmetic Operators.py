eb740398182a0da01fa114feac3f54522d9afcd1
helanwang <christylan1121@gmail.com>
2024-07-26 15:08:34 -0700

    1

---
+++ Python Arithmetic Operators.py
@@ -0,0 +1,52 @@
+
+"""Addition"""
+var1 = 4
+var2 = 10
+var3 = var1 + var2
+print(var3)
+
+"""Subtraction"""
+var1 = 60
+var2 = 30
+var3 = var1 - var2
+print(var3)
+
+
+"""Multiplication"""
+var1 = 25
+var2 = 3
+var3 = var1 * var2
+print(var3)
+
+
+"""Division"""
+var1 = 50
+var2 = 3
+var3 = var1 / var2
+print(var3)
+
+
+"""Floor Division"""
+var1 = 50
+var2 = 3
+var3 = var1 // var2
+print(var3)  # Outputs: 16, it returns to the largest integer
+
+
+"""Exponentiation"""
+var1 = 5
+var2 = 4
+var3 = var1 ** var2  # 5**4
+print(var3)
+
+
+"""Learn the Modulus (%)"""
+var1 = 10
+var2 = 3
+var3 = var1 % var2  # 9/3 = 3. Remainder is 0. 10/3, remainder is 1.
+print(var3)
+
+var1 = 25
+var2 = 5
+var3 = var1 % var2  # Remainder is Zero
+print(var3)
